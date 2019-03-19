17fbf25f1cc000066d8a7dfe1267e1ef3f588622
Mikhail Fomichev <mikhail@fomichev.xyz>
2019-03-19 08:24:34 +0300

    init spring-reactor-postgres-service

---
+++ spring-reactor-postgres-service/src/main/java/xyz/fomichev/spring/reactor/sandbox/postgres/service/Main.java
@@ -0,0 +1,10 @@
+package xyz.fomichev.spring.reactor.sandbox.postgres.service;
+
+/**
+ * @author Mikhail Fomichev (MFomichev)
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Init");
+    }
+}
